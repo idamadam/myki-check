d219cefb38bbe253c99eebc1934e10832f3f0374
Idam Adam <contact@idamadam.com>
2018-09-26 21:26:49 +1000

    Add splash component

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
-import CardBalance from './myki/balance'
-import Login from './myki/login';
+import CardBalance from './containers/balance'
+import Login from './containers/login';
+import Auth from './containers/auth';
+import Splash from './containers/splash';
 
 const RootStack = createStackNavigator(
     {
         Login: Login,
-        Balance: CardBalance
+        Auth: Auth,
+        Balance: CardBalance,
+        Splash: Splash
     },
     {
-        initialRouteName: 'Login',
+        initialRouteName: 'Splash',
         headerMode: 'none',
         cardStyle: {
             backgroundColor: 'white'
