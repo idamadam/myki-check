d9309cbbc3ae83128888bd69f25cabb1c3f9158d
Idam Adam <contact@idamadam.com>
2018-11-24 16:16:00 +1100

    Remove text as import

--- src/containers/balance.js
+++ src/containers/balance.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, AsyncStorage } from 'react-native';
+import { StyleSheet, View, Button, AsyncStorage } from 'react-native';
 import { SecureStore } from 'expo';
 
 import { getData, readData } from '../actions/data'
