538dadc5bc33e1f8ea816be368f28914f7bd5ed5
Idam Adam <contact@idamadam.com>
2018-11-24 16:16:34 +1100

    Remove extraneous components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 import CardBalance from './containers/balance'
@@ -36,13 +35,3 @@ export default class App extends Component {
     render() {
        return  <RootStack />
     }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
