442e9555c014048535793e0141ad161070b3dee8
Idam Adam <contact@idamadam.com>
2018-12-02 10:49:47 +1100

    Delete example stories

--- storybook/stories/Containers/auth.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types';
-import Auth from '../../../src/containers/auth';
-
-export default Auth;
-
-Auth.defaultProps = {
-    
-}
