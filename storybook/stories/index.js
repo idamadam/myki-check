98b4a8772ea9310b8bff257691eeeae0af7818b4
Idam Adam <contact@idamadam.com>
2018-12-02 10:51:15 +1100

    Add all container stories

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
+import Splash from '../../src/containers/splash';
+import Login from '../../src/containers/login';
 import Auth from '../../src/containers/auth';
 import Balance from '../../src/containers/balance';
 
 storiesOf('Containers', module)
+  .add('Splash', () => <Splash />)
+  .add('Login', () => <Login />)
   .add('Auth', () => <Auth/>)
   .add('Balance',() =>  <Balance/>)
