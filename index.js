bb0366dbac6e472dfe59348dfb2c18dbdf2e80bb
Idam Adam <contact@idamadam.com>
2018-12-02 17:33:16 +1100

    Eject Expo

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native'
+import Main from './src/main'
+
+AppRegistry.registerComponent('mykicheck', () => Main);
+
+// import StorybookUI from './storybook';
+
+// export default StorybookUI;
